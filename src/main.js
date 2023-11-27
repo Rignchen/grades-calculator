f7a38714eb11b1cd8b99e8c80089580c8595bae9
Rignchen <rinchendemort@gmail.com>
2023-11-27 13:03:33 +0100

    User can add a grade with the input

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
 import { addGrade } from "./script/addGrade";
+
+// Add a grade when the user clicks on a button
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const grade = parseFloat(
+      button.parentElement.querySelector("div").querySelector("input").value,
+    );
+    // only add a grade if the input is a number between 1 and 6 included with a precision of 0.5
+    if (grade >= 1 && grade <= 6 && grade % 0.5 === 0) {
+      const gradeDiv =
+        button.parentElement.parentElement.parentElement.parentElement.querySelector(
+          "div",
+        );
+      addGrade(grade, gradeDiv);
+    }
+  });
+});
+document.querySelectorAll("input").forEach((input) => {
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const grade = parseFloat(input.value);
+      // only add a grade if the input is a number between 1 and 6 included with a precision of 0.5
+      if (grade >= 1 && grade <= 6 && grade % 0.5 === 0) {
+        const gradeDiv =
+          input.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(
+            "div",
+          );
+        addGrade(grade, gradeDiv);
+      }
+    }
+  });
+});
