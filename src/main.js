b47ba605f213e23d2d6ef9aef34dbd2389450991
Rignchen <rinchendemort@gmail.com>
2023-11-27 14:56:44 +0100

    Limit amount of semester

--- src/main.js
+++ src/main.js
@@ -5,38 +5,6 @@ addSemester();
 // Add a semester when the user clicks on a button
 document.querySelector("#newSemester").addEventListener("click", () => {
   addSemester();
+  if (document.querySelector("#semesterList").childElementCount === 8)
+    document.querySelector("#newSemester").parentElement.remove();
 });
-
-// Add a grade when the user clicks on a button
-/*
-document.querySelectorAll("button").forEach((button) => {
-  button.addEventListener("click", () => {
-    const grade = parseFloat(
-      button.parentElement.querySelector("div").querySelector("input").value,
-    );
-    // only add a grade if the input is a number between 1 and 6 included with a precision of 0.5
-    if (grade >= 1 && grade <= 6 && grade % 0.5 === 0) {
-      const gradeDiv =
-        button.parentElement.parentElement.parentElement.parentElement.querySelector(
-          "div",
-        );
-      addGrade(grade, gradeDiv);
-    }
-  });
-});
-document.querySelectorAll("input").forEach((input) => {
-  input.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      const grade = parseFloat(input.value);
-      // only add a grade if the input is a number between 1 and 6 included with a precision of 0.5
-      if (grade >= 1 && grade <= 6 && grade % 0.5 === 0) {
-        const gradeDiv =
-          input.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(
-            "div",
-          );
-        addGrade(grade, gradeDiv);
-      }
-    }
-  });
-});
-*/
