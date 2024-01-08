8d0f4e6bb8cea175a3836f66ff6519e83e02e6fb
Rignchen <rinchendemort@gmail.com>
2024-01-08 16:22:43 +0100

    get grade from user

--- src/app/semester/semester.component.ts
+++ src/app/semester/semester.component.ts
@@ -2,25 +2,29 @@ import {Component, Input, signal} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {GradeComponent} from "../grade/grade.component";
 import {round} from "../../lib";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-semester',
   standalone: true,
   imports: [
     NgForOf,
-    GradeComponent
+    GradeComponent,
+    FormsModule
   ],
   templateUrl: './semester.component.html'
 })
 export class SemesterComponent {
   @Input() semester!: number[];
   @Input('semester-number') semesterNumber!: number;
-  sum: number = 0;
+  sum: number = 1 + 2 + 3 + 4 + 5 + 6;
+  newGrade!: number;
 
-  addGrade() {
-    let grade = 4;
-    this.semester.push(grade);
-    this.sum += grade;
+  addGrade(grade: number) {
+    if (grade >= 1 && grade <= 6 && grade % 0.5 === 0) {
+      this.semester.push(grade);
+      this.sum += grade;
+    }
   }
 
   protected readonly round = round;
