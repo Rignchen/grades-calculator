71303b5f4bca53b36562fc0569cb41a7cc2225c5
Rignchen <rinchendemort@gmail.com>
2024-01-09 08:04:56 +0100

    calculate average

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,24 @@ import {Component, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SemesterComponent } from "./semester/semester.component";
+import {GradeComponent} from "./grade/grade.component";
+import {FormsModule} from "@angular/forms";
+import {round} from "../lib";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SemesterComponent],
+  imports: [CommonModule, RouterOutlet, SemesterComponent, GradeComponent, FormsModule],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  semesters: number[][] = [[1,2,3,4,5,6],[6,5,4,3,2,1]];
+  semesters: number[][] = [[]];
+  sum = 0;
+  average = 0;
+
+  updateAverage($event: number[]) {
+    this.sum -= $event[0];
+    this.sum += $event[1];
+    this.average = round(this.sum / this.semesters.filter(a => a.length > 0).length, 0.5)
+  }
 }
--- src/app/semester/semester.component.ts
+++ src/app/semester/semester.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, signal} from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {Component, EventEmitter, Input, Output, signal} from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
 import {GradeComponent} from "../grade/grade.component";
 import {round} from "../../lib";
 import {FormsModule} from "@angular/forms";
@@ -10,20 +10,27 @@ import {FormsModule} from "@angular/forms";
   imports: [
     NgForOf,
     GradeComponent,
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './semester.component.html'
 })
 export class SemesterComponent {
   @Input() semester!: number[];
   @Input('semester-number') semesterNumber!: number;
-  sum: number = 1 + 2 + 3 + 4 + 5 + 6;
+  @Output() averageChange = new EventEmitter<number[]>();
+  average = 0;
+  sum: number = 0;
   newGrade!: number;
 
   addGrade(grade: number) {
     if (grade >= 1 && grade <= 6 && grade % 0.5 === 0) {
       this.semester.push(grade);
       this.sum += grade;
+      const old_average = this.average;
+      this.average = round(this.sum / this.semester.length, 0.5);
+      if (old_average !== this.average)
+        this.averageChange.emit([old_average, this.average]);
     }
   }
 
