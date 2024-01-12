a42dc958dfa382d287be1902b9bec51d386320d6
Rignchen <rinchendemort@gmail.com>
2024-01-12 13:53:00 +0100

    extract averages in a new component

--- src/app/average-list/average-list.component.ts
+++ src/app/average-list/average-list.component.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
+import {AverageComponent} from "./average/average.component";
 
 @Component({
   selector: 'app-average-list',
   standalone: true,
-  imports: [],
+  imports: [
+    AverageComponent
+  ],
   templateUrl: './average-list.component.html'
 })
 export class AverageListComponent {
---
+++ src/app/average-list/average/average.component.ts
@@ -0,0 +1,40 @@
+import {Component, Input, OnChanges} from '@angular/core';
+import {NgIf} from "@angular/common";
+import {round} from "../../../lib";
+
+@Component({
+    selector: 'app-average',
+    standalone: true,
+  imports: [
+    NgIf
+  ],
+    templateUrl: './average.component.html'
+})
+export class AverageComponent implements OnChanges {
+  @Input() name!: string;
+  @Input() value!: number;
+  old_value: number = 4;
+  upgrade: number = 0;
+  has_changed: boolean = false;
+
+  defaultClass = "text-xs font-medium"
+  class = this.defaultClass;
+
+  ngOnChanges() {
+    if (this.old_value !== 0) {
+      this.upgrade = (this.value / this.old_value - 1);
+      this.has_changed = true;
+
+      this.class = this.defaultClass;
+      if (this.upgrade > 0) {
+        this.class += " text-green-600";
+      } else if (this.upgrade < 0) {
+        this.class += " text-red-500";
+      }
+    }
+    this.old_value = this.value;
+  }
+
+  protected readonly round = round;
+  protected readonly abs = Math.abs;
+}
