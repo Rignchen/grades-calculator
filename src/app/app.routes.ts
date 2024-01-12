46e3451557157b5a5c5651fb36aaf37f19496329
Rignchen <rinchendemort@gmail.com>
2024-01-12 09:37:19 +0100

    add root aliases

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,9 @@ import {SubjectComponent} from "./subject/subject.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'math', pathMatch: 'full' },
+
+  { path: 'ang' , redirectTo: 'anglais', pathMatch: 'full' },
+  { path: 'english' , redirectTo: 'anglais', pathMatch: 'full' },
+
   { path: ':subject', component: SubjectComponent }
 ];
