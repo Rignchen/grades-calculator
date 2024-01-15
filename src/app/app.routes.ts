3ff8d40c9b27929b193599f8269a3f801a00faa4
Rignchen <rinchendemort@gmail.com>
2024-01-15 11:08:27 +0100

    add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AnglaisComponent } from "./subjects/anglais.component";
 import { SocieteComponent } from "./subjects/societe.component";
 import { EpsicComponent } from "./subjects/epsic.component";
 import { CieComponent } from "./subjects/cie.component";
+import {NotFoundComponent} from "./subjects/404/404.component";
 
 export const routes: Routes = [
   // redirections
@@ -18,4 +19,7 @@ export const routes: Routes = [
   { path: 'societe', component: SocieteComponent },
   { path: 'epsic', component: EpsicComponent },
   { path: 'cie', component: CieComponent },
+
+  // 404
+  { path: '**', component: NotFoundComponent }
 ];
