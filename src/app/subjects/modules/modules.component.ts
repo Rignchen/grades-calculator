b73cc3f4b36dddf89d95f45f3e2628aa48623951
Rignchen <rinchendemort@gmail.com>
2024-03-06 15:43:37 +0100

    grade: use grade component in module page

--- src/app/subjects/modules/modules.component.ts
+++ src/app/subjects/modules/modules.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {GradeComponent} from "../subject/semester/grade/grade.component";
 
 @Component({
   selector: 'app-modules',
   standalone: true,
-  imports: [],
+  imports: [
+    GradeComponent
+  ],
   templateUrl: './modules.component.html'
 })
 export class ModulesComponent {
